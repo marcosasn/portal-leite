a592995356816d737ba7a0082e481a96565d8bcf
Adolfo Melo <adolfo.melo@ccc.ufcg.edu.br>
2015-03-23 10:11:48 -0300

    Atualizando mensagens na view

--- app/models/IDica.java
+++ app/models/IDica.java
@@ -174,6 +174,10 @@ public abstract class IDica implements Comparable<IDica> {
         this.verificaVisualizacao();
     }
 
+	public int getNumeroDenuncias() {
+		return denuncias.size();
+	}
+
     private void verificaVisualizacao() {
         int cont = 0;
         for(Integer value: denuncias.values()) {
